0bfba7c19d975514cfc1e5e49d07ca1210645021
glegendre <kubitusleroi@laposte.net>
2017-07-05 21:14:35 +0200

    main

--- tests/main.c
+++ tests/main.c
@@ -38,7 +38,75 @@ void test_myputchar(void)
 	test_myputcharp2('4');
 	test_myputcharp2('6');
 	test_myputcharp2('q');
-	write(1,"\n\n",2);
+}
+
+void test_mystrnewp2(int size)
+{
+	int i;
+	i = 0;
+	char* str= mystrnew(size);
+	if (str[i] != 0)
+	{
+		write(1,"FAUX",4);
+	}
+	else
+	{
+		while( str[i] == 0)
+		{
+			i++;
+
+		}
+	}
+	write(1,"Vrai!\n",6);
+
+}
+
+void test_mystrnew(void)
+{
+	test_mystrnewp2(6);
+	test_mystrnewp2(9);
+	test_mystrnewp2(13);
+	test_mystrnewp2(0);
+}
+
+
+char* test_mystrdupp2(const char *str)
+{
+	char* r;
+	r = mystrdup(str);
+	int i;
+	i = 0;
+
+	while (str[i] == r[i])
+	{ 
+		if (str[i] == 0 || r[i] == 0)
+		{
+			int x = str[i];
+			int z = r[i];
+
+			if(x-z == 0)
+			{
+				write(1,"Vrai\n",5);
+			}
+			else
+			{
+				write(1,"FAUX\n",5);
+			}
+		}
+		i = i+1;
+	}
+
+
+}
+
+
+void test_mystrdup(void)
+{
+	test_mystrdupp2("lalala");
+	test_mystrdupp2("qsdmkjfhqmksj");
+	test_mystrdupp2("123");
+	test_mystrdupp2(" ");
+
 }
 
 
@@ -46,6 +114,8 @@ int main (void)
 {
 	test_mystrlen();
 	test_myputchar();
+	test_mystrnew();
+	test_mystrdup();
 	return(0);
 }
 
